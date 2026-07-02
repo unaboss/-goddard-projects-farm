9854a14aa1d2c15737aad472400b1e4eb9fb17c3
unaboss <unarinemasiavhula@proton.me>
2026-07-02 20:25:43 +0200

    Increase contrast and saturation on hero carousel images

--- version 5/client/src/pages/Home.jsx
+++ version 5/client/src/pages/Home.jsx
@@ -35,6 +35,7 @@ export default function Home() {
           style={{
             backgroundImage: `url('${img}')`,
             opacity: idx === current ? 1 : 0,
+            filter: 'contrast(1.15) saturate(1.1)',
           }}
         />
       ))}
